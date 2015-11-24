9600139a7f620e50f5a0fb488dab60b9ea84860b
Typhoonspy <typhoon_spy@hotmail.com>
2015-11-24 22:53:57 +0800

    计算器
    计算器

--- index.php
+++ index.php
@@ -2,4 +2,25 @@ phpdemo-doc-试试github怎么玩
 <?php
 phpinfo();
 
+$age = 23;
+if ($age > 25)
+{
+	echo "age > 25";
+}
+else if ($age > 18)
+{
+	echo "25 <= age < 18";
+}
+else
+{
+	echo "age < 18";
+}
+
+echo "<br> end of line";
+
+for ($i = 0;$i < 10;$i++)
+{
+	echo "<br>$i";
+}
+
 ?>
